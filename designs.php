e9c0a5b41f9956e509291fed6183697e7e0e0448
Karuna-1315 <mishrakaruna191@gmail.com>
2026-09-07 21:24:52 +0530

    Add files via upload

---
+++ designs.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Fashion Website</title>
+    <link rel="stylesheet" href="CSS/style.css">
+</head>
+<body>
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="/fashion-designer/designs.php">Designs</a>
+</nav>
+    <h1>Our Fashion Design </h1>
+    <p>Welcome to our fashion collection.</p>
+    <div class="products">
+        <div class="product">
+            <h2>Casual Dress</h2>
+            <p>Comfortable and stylish outfit for daily wear.</p>
+            <p>Price: ₹999</p>
+            <button> View Product</button>
+    </div>
+
+    <div class="product">
+        <h2>Party Wear</h2>
+        <p>Elegant and glamorous outfits for special occasions.</p>
+        <p>Price: ₹1999</p>
+        <button>View Product</button>
+    </div>
+
+    <div class="product">
+        <h2>Wedding Collection</h2>
+        <p>Beautiful traditional outfits for special occasions.</p>
+        <p>Price: ₹2999</p>
+        <button>View Product</button>
+    </div>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Fashion Website</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <nav>
+        <a href="/fashion-designer/index.php">Home</a>
+        <a href="/fashion-designer/designs.php">Designs</a>
+        <a href="#">Categories</a>
+        <a href="#">About</a>
+</nav>
+<div class="hero">
+    <h1>Discover Your Perfect Style</h1>
+    <p>Explore unique fashion designs and discover styles created for every occasion.</p>
+    <a href="designs.php">
+        <button>Explore Now</button>
+    </a>
+</div>
+<section class="categries">
+    <h2>Shop by Categories</h2>
+    <div class="category-list">
+        <div class="category">Casual Wear</div>
+        div class="category">Party Wear</div>
+        <div class="category">Traitional Wear</div>
+        <div class="category">Wedding Collection</div>
+</div>
+</section>
+</body>
+</html>
